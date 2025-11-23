bebd015230619f710e3e0c68b0ea1b032e03f366
MOTOKI-ICHIKAWA <i1chi0ka8wa0@icloud.com>
2025-11-23 23:14:07 +0900

    motoki02

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,7 @@
+// ========================
+// ① jQuery アコーディオン
+// ========================
+
 $(function () {
     $('.faq-accordion-header').click(function () { // .accordion-headerをクリックで発火
         $(this).next().slideToggle();
@@ -5,4 +9,22 @@ $(function () {
         $(this).toggleClass('active');
         // $(this)...$('.accordion-header')に、activeというクラスが追加と削除を交互にする
     });
+});
+
+// CDNでGSAPとScrollTriggerを読み込んだ前提
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll(".top-page-curriculum");
+
+sections.forEach((section) => {
+  const pin = section.querySelector(".top-page-curriculum_pin");
+
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top top",        // セクションが画面上端に来たら
+    end: "+=100%",            // 1画面分スクロールで終了
+    pin: pin,                 // pinに固定
+    pinSpacing: false,        // 次の要素の位置をずらさず重ねる
+    markers: true
+  });
 });
